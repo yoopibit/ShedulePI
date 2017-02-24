abd6ae8d058e1fc80796751803a66bf8dfb1e1ec
Mykhailo Molyboha <viper.jke.mobile@gmail.com>
2017-02-24 21:56:57 +0200

    SS-26 Create and setup git repository Create 3 folders Docs, Site, C# project For C# solution create 4 projects: Models, Models.Test, Services, Services.Test It default initial empty project. Web.API project has default examples Authentication and Value controllers. Look at this examples to undestand as we must change it for our project.

---
+++ SmartScheduler/SmartScheduler.Models.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SmartScheduler.Models.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ SmartScheduler/SmartScheduler.Services/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SmartScheduler.Services.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
