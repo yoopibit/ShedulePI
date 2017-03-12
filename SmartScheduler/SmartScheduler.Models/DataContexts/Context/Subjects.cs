222679ec4866c4ecfb2e5a01183d3262f2768274
Mykhailo Molyboha <viper.jke.mobile@gmail.com>
2017-03-12 18:45:30 +0200

    SS-36 Create Session helper

--- SmartScheduler/SmartScheduler.Models/DataContexts/Context/Subjects.cs
+++ SmartScheduler/SmartScheduler.Models/DataContexts/Context/Subjects.cs
@@ -11,7 +11,8 @@ namespace SmartScheduler.Models.DataContexts.Context
     public interface ISubjectsDbContext
     {
         IEnumerable<Subject> AllSubjects { get; }
-        int AddSubjects(string name);
+        int AddSubjects(string name, int credits = 0);
+        bool UpdateSubject(int id, string newName, int newCredits);
         bool DeleteSubjects(int id);
     }
     public class Subjects : ISubjectsDbContext
@@ -31,14 +32,14 @@ namespace SmartScheduler.Models.DataContexts.Context
             }
         }
 
-        public int AddSubjects(string name)
+        public int AddSubjects(string name, int credits = 0)
         {
             var item = Context.Subjects.FirstOrDefault(x=>x.Name == name);
             if (item != null)
             {
                 return item.SubjectId;
             }
-            item = new DbSubject() { Name = name };
+            item = new DbSubject() { Name = name, Credits = credits };
 
             Context.Subjects.Add(item);
 
@@ -60,6 +61,25 @@ namespace SmartScheduler.Models.DataContexts.Context
             if (item == null) return false;
 
             Context.Subjects.Remove(item);
+            try
+            {
+                Context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Exception:{ex.Message}");
+                return false;
+            }
+            return true;
+        }
+
+        public bool UpdateSubject(int id, string newName, int newCredits)
+        {
+            var item = Context.Subjects.FirstOrDefault(x => x.SubjectId == id);
+            if (item == null) return false;
+
+            item.Name = newName;
+            item.Credits = newCredits;
             try
             {
                 Context.SaveChanges();
