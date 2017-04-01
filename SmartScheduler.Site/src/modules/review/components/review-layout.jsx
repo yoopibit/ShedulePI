6dbfdd94bf8085746cfba1ce311d02058d08272e
TetyanaKandybal <tetyanakandybal@gmail.com>
2017-04-02 00:40:20 +0300

    SS-38 Review page layout improvements - Changed review page design

--- SmartScheduler.Site/src/modules/review/components/review-layout.jsx
+++ SmartScheduler.Site/src/modules/review/components/review-layout.jsx
@@ -7,51 +7,50 @@ import Spinner from './spinner';
 const ReviewLayoutComponent = (props) => ({
 
     weekday : ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+    timeGaps: ["8:30 - 10:00", "10:05 - 11:15", "11:35 - 12:10", "12:45 - 14:00", "14:15 - 15:10"],
 
     componentWillMount: function() {
         this.props.getSchedule();
     },
 
     render: function() {
+        var rows = this.props.schedule && !this.props.isLoading ? this._renderTableRows() : null;
         return this.props.isLoading 
             ? <Spinner /> 
-            : (<div className="l-auth">
-                {this._renderTable()}
-              </div>);
-    },
-
-    _renderTable: function() {
-        var rows = this.props.schedule && !this.props.isLoading ? this._renderTableRows() : null;
-        return (
-            <div className="table">{rows}</div>
-        );
+            : (
+                <div className="table">{rows}</div>
+              );
     },
 
     _renderTableRows: function() {
         var self = this;
         return this._getSortedData().map(function(item) {
-            
             return (
-                <div className="table-row">
-                    <h3 className="week-day">{self._getWeekDay(item.day)}</h3>
-                    {self._renderTableCells(item)}
+                <div className="day-data">
+                   <div className="day-col">
+                        <h3 className="week-day">{self._getWeekDay(item.day)}</h3>
+                    </div>
+                    <div className="table-row">
+                        {self._renderScheduleData(item)}
+                    </div>
                 </div>
             );
         });
     },
 
-    _renderTableCells: function(dayData) {
-     return dayData.data.map(function(item) {
-        return (
-            <div className="table-item">
-                <p>{item.subject}</p>
-                <p>{item.time}</p>
-                <p>{item.place}</p>
-                <p>{item.teacher}</p>             
-            </div>
-        );
-     })
-
+    _renderScheduleData: function(dayData) {
+        return dayData.data.map(function(item) {
+            return (
+                <div className="table-block">
+                    <div className="pair-col">{item.pair}</div>
+                    <div className="data-col">
+                        <p><span className="label">Subject:</span>{item.subject}</p>
+                        <p><span className="label">Place:</span>{item.place}</p>
+                        <p><span className="label">Teacher:</span><i>{item.teacher}</i> </p>            
+                    </div>
+                </div>
+            );
+        })
     },
 
     _getWeekDay: function(index) {
@@ -61,6 +60,12 @@ const ReviewLayoutComponent = (props) => ({
     _getSortedData: function() {
         return this.props.schedule.sort(function(a, b) {
             return a.day - b.day;
+        });
+    },
+
+    _getSortedDataByPair: function(item) {
+        return item.sort(function(a, b) {
+            return a.pair - b.pair;
         });
     }
 });
