b5f673db9e929260330dfcb3b4b5c4964d5c93a2
YuriiKachmar <kachmarani@gmail.com>
2017-04-02 16:49:48 +0300

    TA1201:Add authorization controller - Add some changes

--- SmartScheduler/SmartScheduler.Services/Controllers/AccountController.cs
+++ SmartScheduler/SmartScheduler.Services/Controllers/AccountController.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
-using System.Web.Http
+using System.Web.Http;
 namespace SmartScheduler.Services.Controllers
 {
     public class AccountController : ApiController
